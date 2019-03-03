0c71369492285fac6cad2757b2499aaa3e0b7eb2
sgliput <sgliput@comcast.net>
2019-03-03 16:10:49 -0500

    string replacer function works in command line

---
+++ StringReplacer.java
@@ -0,0 +1,27 @@
+public class StringReplacer {
+
+    public String ReplaceString(String strOrig, String strFind, String strReplace){
+
+        System.out.println();
+        System.out.println("Original String: " + strOrig);
+        System.out.println(); 
+        String[] words=strOrig.split(strFind);//splits the string based on whitespace 
+        // //using java foreach loop to print elements of string array  
+        // System.out.println("Prints out string array of split words");
+        // for(String w:words){  
+        //     System.out.println(w);  
+        // } 
+        String newString = String.join(strReplace, words);
+        System.out.println("New String: " + newString); 
+        return newString;
+        
+    }
+
+	public static void main(String[] args) {
+
+        StringReplacer instance = new StringReplacer();
+        //calling ReplaceString method using instance object
+        instance.ReplaceString("The blue bluebird is blue.", "blue", "red");
+
+	}
+}
